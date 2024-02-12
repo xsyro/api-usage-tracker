d2d3dcc0f972e96e195f0e8e0c5987c15f21cdb3
Igbalajobi Jamiu <kreatifcode@gmail.com>
2024-02-12 22:14:44 +0100

    First, let's give a barebone Spring Initializr

---
+++ src/main/java/com/xsyro/jamiu/ApiUsageTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.xsyro.jamiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiUsageTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiUsageTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xsyro/jamiu/ApiUsageTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xsyro.jamiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsageTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
