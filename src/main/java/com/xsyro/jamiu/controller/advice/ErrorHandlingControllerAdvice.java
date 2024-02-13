4474eab9837c846e8c26bfaa8b10cdfee65bac17
Igbalajobi Jamiu <kreatifcode@gmail.com>
2024-02-13 15:39:02 +0100

    include controller advise to return proper error message on request body validation

--- src/main/java/com/xsyro/jamiu/controller/advice/ErrorHandlingControllerAdvice.java
+++ src/main/java/com/xsyro/jamiu/controller/advice/ErrorHandlingControllerAdvice.java
@@ -1,2 +1,40 @@
-package com.xsyro.jamiu.controller.advice;public class ErrorHandlingControllerAdvice {
+package com.xsyro.jamiu.controller.advice;
+
+
+import com.xsyro.jamiu.model.ValidationErrorResponse;
+import com.xsyro.jamiu.model.Violation;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+class ErrorHandlingControllerAdvice {
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onConstraintValidationException(ConstraintViolationException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (ConstraintViolation violation : e.getConstraintViolations()) {
+            error.getViolations().add(new Violation(violation.getPropertyPath().toString(), violation.getMessage()));
+        }
+        return error;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    ValidationErrorResponse onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ValidationErrorResponse error = new ValidationErrorResponse();
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.getViolations().add(new Violation(fieldError.getField(), fieldError.getDefaultMessage()));
+        }
+        return error;
+    }
 }
